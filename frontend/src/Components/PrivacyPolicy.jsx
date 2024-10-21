a4c8c7390a0fde004efe95b22b92a820926f82db
Saurabhchaudhary9799 <saurbhbailara@gmail.com>
2024-10-22 01:07:20 +0530

    legalterm name changed

--- frontend/src/Components/PrivacyPolicy.jsx
+++ frontend/src/Components/PrivacyPolicy.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const LegalTerms = () => {
+const  PrivacyPolicy= () => {
   return (
     <div className="max-w-4xl mx-auto p-6 overflow-auto h-full">
       <p className='text-xl mb-2 font-bold text-gray-500'>Legal Terms</p>
@@ -69,4 +69,4 @@ const LegalTerms = () => {
   );
 };
 
-export default LegalTerms;
+export default PrivacyPolicy;
